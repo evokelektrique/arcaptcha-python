81644c193806a9623e3b9a33e890d26a649c1097
EVOKE <evoke.lektrique@gmail.com>
2021-11-14 17:17:40 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,19 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
-DESCRIPTION = 'My first Python package'
-LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+VERSION = '1.0.0'
+DESCRIPTION = 'Python package for Arcaptcha'
+LONG_DESCRIPTION = 'Validate and display captcha for Arcaptcha'
 
 # Setting up
 setup(
   # the name must match the folder name 'verysimplemodule'
-  name="verysimplemodule",
+  name="arcaptcha",
   version=VERSION,
-  author="Jason Dsouza",
-  author_email="<youremail@email.com>",
+  author="EVOKE",
+  author_email="evoke.lektrique@gmail.com",
   description=DESCRIPTION,
   long_description=LONG_DESCRIPTION,
   packages=find_packages(),
-  install_requires=[], # add any additional packages that
-  # needs to be installed along with your package. Eg: 'caer'
-
-  keywords=['python', 'first package'],
-  classifiers= [
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Education",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 3",
-    "Operating System :: MacOS :: MacOS X",
-    "Operating System :: Microsoft :: Windows",
-  ]
+  install_requires=["requests"],
+  keywords=['captcha', 'api'],
 )
